c1b31b0bb16c64b8abcf6093ee9e803d5c2eccf0
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-04-18 10:16:06 +0700

    get all student

--- src/app/service/student.service.ts
+++ src/app/service/student.service.ts
@@ -1,9 +1,19 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {environment} from '../../environments/environment';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Student} from '../model/student';
+
+const API_URL = `${environment.apiUrl}`;
 
 @Injectable({
   providedIn: 'root'
 })
 export class StudentService {
+  constructor(private http: HttpClient) {
+  }
 
-  constructor() { }
+  getAllStudents(): Observable<Student[]> {
+    return this.http.get<Student[]>(API_URL + '/students');
+  }
 }
