670056c9f33635492785a428aa665a8cc89f56e7
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-04-20 10:07:46 +0700

    add env prod

--- src/app/coach/coach-routing.module.ts
+++ src/app/coach/coach-routing.module.ts
@@ -1,12 +1,14 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {ListCoachComponent} from './list-coach/list-coach.component';
+import {AdminAuthGuard} from '../helper/admin-auth-guard';
 
 
 const routes: Routes = [
   {
     path: '',
-    component: ListCoachComponent
+    component: ListCoachComponent,
+    canActivate: [AdminAuthGuard]
   }
 ];
 
