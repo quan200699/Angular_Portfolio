f7df49d7eb6f28885552b436825ff41b8b30d1e3
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-04-20 12:54:59 +0700

    add new npm express server

--- server.js
+++ server.js
@@ -7,10 +7,23 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/ePortfolio'));
 
-app.get('/*', function(req,res) {
+app.get('/*', function (req, res) {
 
-  res.sendFile(path.join(__dirname,'/dist/ePortfolio/index.html'));
+  res.sendFile(path.join('ePortfolio', '/dist/ePortfolio/index.html'));
 });
+
+const forceSSL = function () {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+        ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+}
+
+app.use(forceSSL());
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
