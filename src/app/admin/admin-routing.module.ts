f77cd7253a3af7f04ec6970b6f07e5e5f2db6b76
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-04-20 13:23:59 +0700

    deploy heroku done

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,6 +1,7 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {AdminAuthGuard} from '../helper/admin-auth-guard';
+import {ListCoachComponent} from '../coach/list-coach/list-coach.component';
 
 
 const routes: Routes = [
@@ -8,6 +9,11 @@ const routes: Routes = [
     path: 'student-management',
     canActivate: [AdminAuthGuard],
     loadChildren: () => import('../student/student.module').then(module => module.StudentModule)
+  },
+  {
+    path: 'coach-management',
+    component: ListCoachComponent,
+    canActivate: [AdminAuthGuard]
   }
 ];
 
--- src/app/coach/coach-routing.module.ts
+++ src/app/coach/coach-routing.module.ts
@@ -1,16 +1,7 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {ListCoachComponent} from './list-coach/list-coach.component';
-import {AdminAuthGuard} from '../helper/admin-auth-guard';
 
-
-const routes: Routes = [
-  {
-    path: '',
-    component: ListCoachComponent,
-    canActivate: [AdminAuthGuard]
-  }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
