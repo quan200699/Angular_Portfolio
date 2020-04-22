02ca644355ef6e71e3747a20ffaf289b9444348d
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-04-22 09:01:39 +0700

    create student done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import {LoginComponent} from './login/login.component';
-import {ReactiveFormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {JwtInterceptor} from './helper/jwt-interceptor';
 import {ErrorInterceptor} from './helper/error-interceptor';
 import { NavbarComponent } from './shared/navbar/navbar.component';
@@ -23,6 +23,7 @@ import { SidebarComponent } from './shared/sidebar/sidebar.component';
     AppRoutingModule,
     HttpClientModule,
     ReactiveFormsModule,
+    FormsModule
   ],
   providers: [
     {provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true},
--- src/app/student/student-routing.module.ts
+++ src/app/student/student-routing.module.ts
@@ -1,12 +1,17 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {ListStudentComponent} from './list-student/list-student.component';
+import {CreateStudentComponent} from './create-student/create-student.component';
 
 
 const routes: Routes = [
   {
     path: '',
     component: ListStudentComponent
+  },
+  {
+    path: 'create',
+    component: CreateStudentComponent
   }
 ];
 
