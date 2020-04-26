ace86ef3db6c0c54e884961b3318a4e37686b774
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-04-26 14:40:45 +0700

    add outcome.service.ts

---
+++ src/app/service/outcome.service.ts
@@ -0,0 +1,36 @@
+import {Injectable} from '@angular/core';
+import {environment} from '../../environments/environment';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Outcome} from '../model/outcome';
+
+const API_URL = `${environment.apiUrl}`;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OutcomeService {
+
+  constructor(private http: HttpClient) {
+  }
+
+  getAllOutcome(): Observable<Outcome[]> {
+    return this.http.get<Outcome[]>(API_URL + '/outcomes');
+  }
+
+  createNewOutcome(outcome: Outcome): Observable<Outcome> {
+    return this.http.post<Outcome>(API_URL + '/outcomes', outcome);
+  }
+
+  getOutcome(id: number): Observable<Outcome> {
+    return this.http.get<Outcome>(API_URL + `/outcomes/${id}`);
+  }
+
+  editOutcome(id: number, outcome: Outcome): Observable<Outcome> {
+    return this.http.put<Outcome>(API_URL + `/outcomes/${id}`, outcome);
+  }
+
+  deleteOutcome(id: number): Observable<Outcome> {
+    return this.http.delete<Outcome>(API_URL + `/outcomes/${id}`);
+  }
+}
