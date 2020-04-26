9313aabf5adc2651adb13bc68ac0cbe168eb2491
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-04-26 16:23:11 +0700

    add skill.module.ts

---
+++ src/app/skill/skill.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {SkillRoutingModule} from './skill-routing.module';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SkillRoutingModule
+  ]
+})
+export class SkillModule { }
