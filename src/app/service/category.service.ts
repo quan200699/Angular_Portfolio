538d35a6b0abd56183cbec77b3c66a49b3975adc
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-04-26 15:50:56 +0700

    add category.service.ts

---
+++ src/app/service/category.service.ts
@@ -0,0 +1,36 @@
+import {Injectable} from '@angular/core';
+import {environment} from '../../environments/environment';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Category} from '../model/category';
+
+const API_URL = `${environment.apiUrl}`;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoryService {
+
+  constructor(private http: HttpClient) {
+  }
+
+  getAllCategory(): Observable<Category[]> {
+    return this.http.get<Category[]>(API_URL + '/categories');
+  }
+
+  createNewCategory(category: Category): Observable<Category> {
+    return this.http.post<Category>(API_URL + '/categories', category);
+  }
+
+  getCategory(id: number): Observable<Category> {
+    return this.http.get<Category>(API_URL + `/categories/${id}`);
+  }
+
+  editCategory(id: number, category: Category): Observable<Category> {
+    return this.http.put<Category>(API_URL + `/categories/${id}`, category);
+  }
+
+  deleteCategory(id: number): Observable<Category> {
+    return this.http.delete<Category>(API_URL + `/categories/${id}`);
+  }
+}
