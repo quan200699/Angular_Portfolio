64a71e7b5f255f9db54ba416c399987b0899037f
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-04-29 22:03:20 +0700

    test coach service return a list passed!

---
+++ src/app/service/coach.service.spec.ts
@@ -0,0 +1,64 @@
+import { TestBed, async, inject } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import {environment} from '../../environments/environment';
+import {CoachService} from './coach.service';
+
+const API_URL = `${environment.apiUrl}`;
+
+describe('CoachService', () => {
+  let coachService: CoachService;
+  let httpMock: HttpTestingController;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+      ],
+      providers: [
+        CoachService
+      ],
+    });
+
+    coachService = TestBed.get(CoachService);
+    httpMock = TestBed.get(HttpTestingController);
+  });
+
+
+
+  it(`should return a list coach`, async(inject([HttpTestingController, CoachService],
+    (httpClient: HttpTestingController, coachService: CoachService) => {
+
+      const coachList = [
+        {
+          'coachId': '001',
+          'id': 1,
+          'name': 'Nguyen Minh Quan',
+          'email': 'quan.nguyen@codegym.vn'
+        },
+        {
+          'coachId': '002',
+          'id': 2,
+          'name': 'Vu Kieu Anh',
+          'email': 'anh.vu@codegym.vn'
+        },
+        {
+          'coachId': '003',
+          'id': 3,
+          'name': 'Dang Huy Hoa',
+          'email': 'hoa.dang@codegym.vn'
+        }
+      ];
+
+
+      coachService.getAllCoach()
+        .subscribe((coach: any) => {
+          expect(coach.length).toBe(3);
+        });
+
+      let req = httpMock.expectOne(API_URL + '/coaches');
+      expect(req.request.method).toBe('GET');
+
+      req.flush(coachList);
+      httpMock.verify();
+
+    })));
+});
