7d54b5cd4f317b76ce30e1bbe75ec5e1f979bf8e
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-04-29 22:04:41 +0700

    refactor

--- src/app/service/coach/coach.service.spec.ts
+++ src/app/service/coach/coach.service.spec.ts
@@ -1,11 +1,32 @@
-import { TestBed, async, inject } from '@angular/core/testing';
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import {TestBed, async, inject} from '@angular/core/testing';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
 import {environment} from '../../../environments/environment';
 import {CoachService} from './coach.service';
 
 const API_URL = `${environment.apiUrl}`;
 
 describe('CoachService', () => {
+
+  const coachList = [
+    {
+      'coachId': '001',
+      'id': 1,
+      'name': 'Nguyen Minh Quan',
+      'email': 'quan.nguyen@codegym.vn'
+    },
+    {
+      'coachId': '002',
+      'id': 2,
+      'name': 'Vu Kieu Anh',
+      'email': 'anh.vu@codegym.vn'
+    },
+    {
+      'coachId': '003',
+      'id': 3,
+      'name': 'Dang Huy Hoa',
+      'email': 'hoa.dang@codegym.vn'
+    }
+  ];
   let coachService: CoachService;
   let httpMock: HttpTestingController;
   beforeEach(() => {
@@ -21,34 +42,8 @@ describe('CoachService', () => {
     coachService = TestBed.get(CoachService);
     httpMock = TestBed.get(HttpTestingController);
   });
-
-
-
   it(`should return a list coach`, async(inject([HttpTestingController, CoachService],
     (httpClient: HttpTestingController, coachService: CoachService) => {
-
-      const coachList = [
-        {
-          'coachId': '001',
-          'id': 1,
-          'name': 'Nguyen Minh Quan',
-          'email': 'quan.nguyen@codegym.vn'
-        },
-        {
-          'coachId': '002',
-          'id': 2,
-          'name': 'Vu Kieu Anh',
-          'email': 'anh.vu@codegym.vn'
-        },
-        {
-          'coachId': '003',
-          'id': 3,
-          'name': 'Dang Huy Hoa',
-          'email': 'hoa.dang@codegym.vn'
-        }
-      ];
-
-
       coachService.getAllCoach()
         .subscribe((coach: any) => {
           expect(coach.length).toBe(3);
@@ -59,6 +54,5 @@ describe('CoachService', () => {
 
       req.flush(coachList);
       httpMock.verify();
-
     })));
 });
