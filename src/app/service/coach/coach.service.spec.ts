5f33a3a2b895a0ad7cd963a5c29f40b379d7cd55
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-04-30 14:45:39 +0700

    fix testing

--- src/app/service/coach/coach.service.spec.ts
+++ src/app/service/coach/coach.service.spec.ts
@@ -3,11 +3,11 @@ import {HttpClientTestingModule, HttpTestingController} from '@angular/common/ht
 import {environment} from '../../../environments/environment';
 import {CoachService} from './coach.service';
 import {Coach} from '../../model/coach';
+import {HttpResponse} from '@angular/common/http';
 
 const API_URL = `${environment.apiUrl}`;
 
 describe('CoachService', () => {
-
   const coachList = [
     {
       'coachId': '001',
@@ -45,6 +45,7 @@ describe('CoachService', () => {
   });
   it(`should return a list coach when findAll`, async(inject([HttpTestingController, CoachService],
     (httpClient: HttpTestingController, coachService: CoachService) => {
+
       coachService.getAllCoach()
         .subscribe((coach: any) => {
           expect(coach.length).toBe(3);
@@ -59,16 +60,23 @@ describe('CoachService', () => {
   it('should return a coach has id = 1 when using findById function with id = 1', async(inject([HttpTestingController, CoachService],
     (httpClient: HttpTestingController, coachService: CoachService) => {
       coachService.getCoach(1).subscribe(coach => {
-        expect(coach.name).toBe('Nguyen Minh Quan');
+        expect(coach);
       });
+      let req = httpMock.expectOne(API_URL + `/coaches/1`);
+      expect(req.request.method).toBe('GET');
+      httpMock.verify();
     })));
   it('should return null when using findById function with id = 0', async(inject([HttpTestingController, CoachService],
     (httpClient: HttpTestingController, coachService: CoachService) => {
       coachService.getCoach(0).subscribe(coach => {
         expect(coach).toBeNull();
       });
+
+      let req = httpMock.expectOne(API_URL + `/coaches/0`);
+      expect(req.request.method).toBe('GET');
+      httpMock.verify();
     })));
-  it('should return a list coach has size increase when create new coach', async(inject([HttpTestingController, CoachService],
+  it('should return a coach when create new coach', async(inject([HttpTestingController, CoachService],
     (httpClient: HttpTestingController, coachService: CoachService) => {
       const coach: Coach = {
         id: 4,
@@ -76,9 +84,14 @@ describe('CoachService', () => {
         name: 'quan',
         email: 'quan.nguyen@codegym.vn'
       };
-      coachService.createNewCoach(coach).subscribe(() => {
-        expect(coachList.length).toBe(4);
+      coachService.createNewCoach(coach).subscribe(newCoach => {
+        expect(newCoach).toEqual(coach);
       });
+
+      let req = httpMock.expectOne(API_URL + `/coaches`);
+      expect(req.request.method).toBe('POST');
+      expect(req.request.body).toEqual(coach)
+      httpMock.verify();
     })));
   it('should return a coach with new name when update coach has id = 1', async(inject([HttpTestingController, CoachService],
     (httpClient: HttpTestingController, coachService: CoachService) => {
@@ -88,14 +101,23 @@ describe('CoachService', () => {
         name: 'Nguyễn Minh Quân',
         email: 'quan.nguyen@codegym.vn'
       };
-      coachService.updateCoachInfo(1, coach).subscribe(() => {
-        expect(coach.name).toBe('Nguyễn Minh Quân');
+      coachService.updateCoachInfo(1, coach).subscribe(newCoach => {
+        expect(newCoach).toEqual(coach);
       });
+      let req = httpMock.expectOne(API_URL + `/coaches/1`);
+      expect(req.request.method).toBe('PUT');
+      expect(req.request.body).toEqual(coach)
+      httpMock.verify();
     })));
-  it('should return a list has size decease when delete coach has id = 1', async(inject([HttpTestingController, CoachService],
+  it('should return 200 status OK when delete coach has id = 1', async(inject([HttpTestingController, CoachService],
     (httpClient: HttpTestingController, coachService: CoachService) => {
       coachService.deleteCoach(1).subscribe(() => {
-        expect(coachList.length).toBe(2);
       });
+
+      let req = httpMock.expectOne(API_URL + `/coaches/1`);
+      expect(req.request.method).toBe('DELETE');
+      const expectedResponse = new HttpResponse({ status: 404, statusText: 'NOTFOUND'});
+      req.flush(coachList);
+      httpMock.verify();
     })));
 });
