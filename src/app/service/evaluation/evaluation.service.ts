4c16a75c8f34cf1eda878f192f5cbfb7da1d7be1
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-05-04 17:16:19 +0700

    evaluation.service.ts done

--- src/app/service/evaluation/evaluation.service.ts
+++ src/app/service/evaluation/evaluation.service.ts
@@ -1,9 +1,36 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {environment} from '../../../environments/environment';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Evaluations} from '../../model/evaluations';
+
+const API_URL = `${environment.apiUrl}`;
 
 @Injectable({
   providedIn: 'root'
 })
 export class EvaluationService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
+
+  getAllEvaluation(): Observable<Evaluations[]> {
+    return this.http.get<Evaluations[]>(API_URL + '/evaluations');
+  }
+
+  getEvaluation(id: number): Observable<Evaluations> {
+    return this.http.get<Evaluations>(API_URL + `/evaluations/${id}`);
+  }
+
+  createNewEvaluation(evaluations: Evaluations): Observable<Evaluations> {
+    return this.http.post<Evaluations>(API_URL + '/evaluations', evaluations);
+  }
+
+  updateEvaluationInfo(id: number, evaluations: Evaluations): Observable<Evaluations> {
+    return this.http.put(API_URL + `/evaluations/${id}`, evaluations);
+  }
+
+  deleteEvaluation(id: number): Observable<Evaluations> {
+    return this.http.delete<Evaluations>(API_URL + `/evaluations/${id}`);
+  }
 }
