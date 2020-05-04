c62ea279aac61f31809a42edac438cb4fb22a1e8
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-05-04 17:09:41 +0700

    add evaluations.ts

---
+++ src/app/model/evaluations.ts
@@ -0,0 +1,11 @@
+export interface Evaluations {
+  id?: number;
+  name?: string;
+  description?: string;
+  evaluation?: string;
+  createDate?: Date;
+  student?: any;
+  templates?: any;
+  coach?: any;
+  classes?: any;
+}
