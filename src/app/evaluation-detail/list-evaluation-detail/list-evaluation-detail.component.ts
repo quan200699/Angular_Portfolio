39938c24c07c9fd47c6eedb06bcc452414476b1e
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-05-06 09:40:33 +0700

    list evaluation detail done

--- src/app/evaluation-detail/list-evaluation-detail/list-evaluation-detail.component.ts
+++ src/app/evaluation-detail/list-evaluation-detail/list-evaluation-detail.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {EvaluationDetail} from '../../model/evaluation-detail';
+import {EvaluationDetailService} from '../../service/evaluation-detail/evaluation-detail.service';
+
+declare var $: any;
 
 @Component({
   selector: 'app-list-evaluation-detail',
@@ -6,10 +10,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-evaluation-detail.component.css']
 })
 export class ListEvaluationDetailComponent implements OnInit {
+  evaluationDetailList: EvaluationDetail[];
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private evaluationDetailService: EvaluationDetailService) {
   }
 
+  ngOnInit() {
+    this.getAllEvaluationDetail();
+  }
+
+  getAllEvaluationDetail() {
+    return this.evaluationDetailService.getAllEvaluationDetail().subscribe(evaluationDetailList => {
+      this.evaluationDetailList = evaluationDetailList;
+      $(function() {
+        $('#table-evaluation-detail').DataTable({
+          'paging': true,
+          'lengthChange': false,
+          'searching': false,
+          'ordering': true,
+          'info': true,
+          'autoWidth': false,
+        });
+      });
+    });
+  }
 }
