f99c94787fbb110125452848a4cd00d19ccb48a1
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-05-06 15:17:32 +0700

    add message

--- src/app/evaluation-detail/create-evaluation-detail/create-evaluation-detail.component.ts
+++ src/app/evaluation-detail/create-evaluation-detail/create-evaluation-detail.component.ts
@@ -76,6 +76,19 @@ export class CreateEvaluationDetailComponent implements OnInit {
       for (let evaluationDetail of this.listEvaluationDetail) {
         this.createEvaluationDetail(evaluationDetail);
       }
+      $(function() {
+        const Toast = Swal.mixin({
+          toast: true,
+          position: 'top-end',
+          showConfirmButton: false,
+          timer: 3000
+        });
+
+        Toast.fire({
+          type: 'success',
+          title: 'Tạo mới thành công'
+        });
+      });
     } else {
       $(function() {
         const Toast = Swal.mixin({
