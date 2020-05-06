3f372e99984313a01508f53a23194dc734548bc5
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-05-06 09:13:13 +0700

    add evaluation-detail.module.ts

---
+++ src/app/evaluation-detail/evaluation-detail.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {EvaluationDetailRoutingModule} from './evaluation-detail-routing.module';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    EvaluationDetailRoutingModule
+  ]
+})
+export class EvaluationDetailModule { }
